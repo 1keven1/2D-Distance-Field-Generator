4ef26b7f611e6e5960b7af73b25fa75cd23632b4
Suzhi_Zhang <zhangsuzhe@sina.com>
2021-07-28 16:13:16 +0800

    更新日志： 1. 修复获取圆环内点算法会抛弃右侧和下册点的bug； 2. 现在将图片文件拖动到执行文件上就可以运行； 3. 更新了进度条；

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ctime>
-#define SQRT2 1.41421356
+#include <string>
 
-constexpr int spreadFactor = 64;
+constexpr auto SQRT2 = 1.41421356;
+constexpr int spreadFactor = 32;
 constexpr int searchingRingWidth = 10;
 
 cv::Mat srcImg;
 cv::Size srcSize;
 cv::Mat desImg;
 
+inline void UpdateProgress(float progress)
+{
+	int barWidth = 70;
+
+	std::cout << "[";
+	int pos = barWidth * progress;
+	for (int i = 0; i < barWidth; ++i) {
+		if (i < pos) std::cout << "=";
+		else if (i == pos) std::cout << ">";
+		else std::cout << " ";
+	}
+	std::cout << "] " << int(progress * 100.0) << " %\r";
+	std::cout.flush();
+};
 
 // 读取图片
 cv::Mat ReadImage(const cv::String& fileName)
@@ -73,11 +88,9 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 	assert(radiusSmall >= 0);
 
 	std::vector<cv::Point2i> points;
-	int squareRadiusSmall = radiusSmall * radiusSmall;
 	// 坐标轴上的
 	for (int i = radiusSmall + 1; i <= radiusBig; i++)
 	{
-		if (i + center.x < 0 || i + center.x >= matSize.width) continue;
 		points.push_back(cv::Point2i(i, 0));
 	}
 
@@ -86,7 +99,6 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 	int maxXBig = floor(radiusBig / SQRT2);
 	for (int i = maxXSmall + 1; i <= maxXBig; i++)
 	{
-		if (i + center.x < 0 || i + center.x >= matSize.width ||i + center.y < 0 || i + center.y >= matSize.height) continue;
 		points.push_back(cv::Point2i(i, i));
 	}
 
@@ -114,10 +126,7 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 			if (i > maxYBottomSmall && i <= maxXSmall)
 			{
 				if (j <= xMaxUpperSmall) continue;
-				//if ((j * j + i * i) < squareRadiusSmall) continue;
 			}
-
-			if (j + center.x < 0 || j + center.x >= matSize.width || i + center.y < 0 || i + center.y >= matSize.height) continue;
 			points.push_back(cv::Point2i(j, i));
 		}
 	}
@@ -137,10 +146,7 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 			if (i <= maxXSmall)
 			{
 				if (j <= xMaxUpperSmall) continue;
-				//if ((j * j + i * i) < squareRadiusSmall) continue;
 			}
-
-			if (j + center.x < 0 || j + center.x >= matSize.width || i + center.y < 0 || i + center.y >= matSize.height) continue;
 			points.push_back(cv::Point2i(j, i));
 		}
 	}
@@ -153,7 +159,6 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 	{
 		if (points[i].x != points[i].y)
 		{
-			if (points[i].y + center.x < 0 || points[i].y + center.x >= matSize.width || points[i].x + center.y < 0 || points[i].x + center.y >= matSize.height) continue;
 			points.push_back(cv::Point2i(points[i].y, points[i].x));
 		}
 	}
@@ -164,7 +169,6 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 	{
 		if (points[i].x != 0)
 		{
-			if (-points[i].x + center.x < 0 || -points[i].x + center.x >= matSize.width || points[i].y + center.y < 0 || points[i].y + center.y >= matSize.height) continue;
 			points.push_back(cv::Point2i(-points[i].x, points[i].y));
 		}
 	}
@@ -175,28 +179,31 @@ std::vector<cv::Point2i> GetAllPointInRing(const cv::Point2i& center, const int&
 	{
 		if (points[i].y != 0)
 		{
-			if (points[i].x + center.x < 0 || points[i].x + center.x >= matSize.width || -points[i].y + center.y < 0 || -points[i].y + center.y >= matSize.height) continue;
 			points.push_back(cv::Point2i(points[i].x, -points[i].y));
 		}
 	}
 
-	// 平移到圆心
+
+	// 平移到圆心，如果不在范围内则抛弃
+	std::vector<cv::Point2i> newPoints;
 	for (auto& point : points)
 	{
 		point.x += center.x;
 		point.y += center.y;
+		if (point.x < 0 || point.x >= matSize.width || point.y < 0 || point.y >= matSize.height) continue;
+		newPoints.push_back(point);
 	}
-	return points;
+
+	return newPoints;
 }
 
 void GenerateDistanceField(const cv::Mat& src, cv::Mat des, const int& spreadFactor)
 {
 	int squareSpreadFactor = pow(spreadFactor, 2);
-	// TODO: 生成距离场的算法
 	// 对每个像素
-	for (int i = 0; i < src.rows; i++)
+	for (int i = 0; i < srcSize.height; i++)
 	{
-		for (int j = 0; j < src.cols; j++)
+		for (int j = 0; j < srcSize.width; j++)
 		{
 			// 船新算法
 			cv::Point self(j, i);
@@ -211,7 +218,7 @@ void GenerateDistanceField(const cv::Mat& src, cv::Mat des, const int& spreadFac
 				std::vector<cv::Point2i> neighbors;
 				neighbors = GetAllPointInRing(self, t, tBig, srcSize);
 				bool bFound = false;
-				for (auto neighbor : neighbors)
+				for (const auto& neighbor : neighbors)
 				{
 					// 如果白色找到了黑像素
 					if (src.at<cv::Vec3b>(neighbor)[0] < 128 && bWhite)
@@ -244,35 +251,48 @@ void GenerateDistanceField(const cv::Mat& src, cv::Mat des, const int& spreadFac
 			// 写入目标图片
 			des.at<uchar>(self) = finalResult * 255;
 		}
-		if (i % 100 == 0)
+		if (i % 10 == 0)
 		{
-			std::cout << i << std::endl;
+			UpdateProgress(static_cast<float>(i) / srcSize.height);
 		}
 	}
+	UpdateProgress(1);
 }
 
-int main()
+
+int main(int argc, char* argv[])
 {
-	cv::String filePath = "gan1024.png";
-	srcImg = ReadImage(filePath);
-	srcSize = cv::Size(srcImg.cols, srcImg.rows);
-	desImg = cv::Mat(srcSize, CV_8UC1, uchar(0));
-	
-	clock_t start = clock();
-	GenerateDistanceField(srcImg, desImg, spreadFactor);
-	clock_t end = clock();
-	std::cout << end - start << "ms" << std::endl;
+	std::cout << "共有" << argc - 1 << "个文件需要处理" << std::endl;
+	std::cout << "-----------------------------START-----------------------------" << std::endl;
+	for (int i = 1; i < argc; i++)
+	{
+		// 检查文件格式
+		std::string filePath = argv[i];
+		std::string fileType = filePath.substr(filePath.find_last_of("."), filePath.length());
+		std::string fileName = filePath.substr(filePath.find_last_of("\\") + 1, filePath.length());
+		if (fileType != ".png" && fileType != ".jpg" && fileType != ".jpeg" && fileType != ".PNG" && fileType != ".JPG" && fileType != ".JPEG")
+		{
+			std::cout << "文件" << fileName << "格式有误" << std::endl;
+			continue;
+		}
 
-	cv::cvtColor(srcImg, srcImg, cv::COLOR_RGB2BGR);
-	cv::resize(srcImg, srcImg, cv::Size(700, 700));
-	cv::resize(desImg, desImg, cv::Size(256, 256));
+		std::cout << "正在处理第" << i << "个文件：" << fileName << std::endl;
+		srcImg = ReadImage(filePath);
+		srcSize = cv::Size(srcImg.cols, srcImg.rows);
+		desImg = cv::Mat(srcSize, CV_8UC1, uchar(0));
 
-	//cv::imshow("src", srcImg);
-	//cv::imshow("des", desImg);
-	cv::imwrite("Result_Gan_DF64_256_1.png", desImg);
-	//cv::resize(desImg, desImg, cv::Size(128, 128));
-	//cv::imwrite("Result_Gan_DF64_128.png", desImg);
+		clock_t start = clock();
+		GenerateDistanceField(srcImg, desImg, spreadFactor);
+		clock_t end = clock();
 
-	cv::waitKey(0);
+		std::cout << std::endl << "耗时" << static_cast<float>(end - start) / 1000 << "s" << std::endl << std::endl;
+
+		// 储存文件
+		int n = filePath.find_last_of(".");
+		std::string DesName = filePath.substr(0, n) + "_DF.png";
+		cv::imwrite(DesName, desImg);
+	}
+
+	system("pause");
 	return 0;
 }
